175e12b62d9fa7f824a1c7792cc76fbfa7d25e35
Susy(Qinqin) Yang <susyyang24@gmail.com>
2021-02-02 17:26:58 -0700

    router is connected

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,9 +1,41 @@
 import React from "react";
+import SearchHome from './SearchHome.jsx';
+import SearchResults from './SearchResults.jsx';
+import {
+    HashRouter as Router,
+    // BrowserRouter as Router,
+    Switch,
+    Route,
+    NavLink,
+    Redirect
+} from 'react-router-dom';
 
 const App = (props) => {
     return (
-        <div>I am in App</div>
+        <Router >
+            <div>
+                <div className='header'>
+                    <h1 className='page-title'>Search Github Users</h1>
+                    <div className='nav'>
+                        <NavLink to='/search' className='nav-home'>
+                            Home
+                        </NavLink>
+                    </div>
+                </div>
+
+                <div className='home-page-bdy'>
+                    <Switch>
+                        <Route exact path='/search' component={SearchHome}/>
+                        <Redirect 
+                        exact from="/search/results/"
+                        to="/search/results/susyyang"/>
+                        <Route path="/search/results/:user" component={SearchResults} />
+                    </Switch>
+                </div>
+
+            </div>
+        </Router>
     )
-}
+};
 
 export default App;
---
+++ client/src/Components/SearchHome.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchHome = () => {
+    return (
+        <div>Search Home</div>
+    )
+}
+
+export default SearchHome;
